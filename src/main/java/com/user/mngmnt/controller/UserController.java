1ee5ab6ae69ca090a668b29ff135f5413a005b0e
rakeshsonimca <rakeshsonimca@gmail.com>
2023-03-31 15:27:00 +0530

    Reverted old code heb-phm-10

--- src/main/java/com/user/mngmnt/controller/UserController.java
+++ src/main/java/com/user/mngmnt/controller/UserController.java
@@ -25,9 +25,6 @@ import java.util.Collection;
 import java.util.List;
 
 
-//import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
-
-
 @Controller
 public class UserController {
 
@@ -94,33 +91,12 @@ public class UserController {
         System.out.println("Herbert Tito Tan");
         System.out.println("Roel Angeles");
         System.out.println("Sajana Vijayan");
-
-        for(int i=0;i<3;i++){
-            System.out.println("....Hello World By Varsha Suresh...");
-        }
-
-
-        
-        for (int i=0; i<3; i++){
-            System.out.println("Hello World By Roel Angeles");
-        }
-
-
-
-        for (int i=0; i<3; i++){
-            System.out.println("Hello world by Mark Bryan Calupig");
-        }
-
-
         for (int i = 0; i < 3; i++) {
             System.out.println("Hello world By Edmark Argente");
         }
         for (int i = 0; i <= 3; i++) {
             System.out.println("Hello world By Rakesh Soni");
         }
-
-
-
         return modelAndView;
     }
 
