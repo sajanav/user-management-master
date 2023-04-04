d88f1f1829ae3bf85fb96e4d540a4ed874c31de4
MACintoshPH <argenteedmarkr@gmail.com>
2023-04-04 14:33:23 +0800

    heb-phm-junit-edmark -> need junit test case for searchBy and it should be success.

---
+++ src/test/java/com/user/mngmnt/service/UserServiceTest.java
@@ -0,0 +1,49 @@
+package com.user.mngmnt.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.user.mngmnt.model.User;
+import com.user.mngmnt.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@DisplayName("Test class for User Service")
+class UserServiceImplTest {
+
+    @InjectMocks
+    private UserService userService = new UserServiceImpl();
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testSearchBy_searchByKeywordAndCategory_successfulSearched() {
+        List<User> users = new ArrayList<>();
+        String[] criteria = {"firstName", "lastName", "email"};
+        User user = new User("Edmark","Argente","edmark.argente@ust.com","","",true);
+        users.add(user);
+
+        Mockito.when(userRepository.findByFirstNameIgnoreCaseContaining(Mockito.anyString())).thenReturn(users);
+        Mockito.when(userRepository.findByLastNameIgnoreCaseContaining(Mockito.anyString())).thenReturn(users);
+        Mockito.when(userRepository.findByEmailIgnoreCaseContaining(Mockito.any())).thenReturn(users);
+
+        assertEquals(userService.searchBy(Mockito.anyString(),criteria[0]).get(0).getFirstName(), user.getFirstName());
+        assertEquals(userService.searchBy(Mockito.anyString(),criteria[1]).get(0).getLastName(), user.getLastName());
+        assertEquals(userService.searchBy(Mockito.anyString(),criteria[2]).get(0).getEmail(), user.getEmail());
+        assertNotNull(userService.searchBy(Mockito.anyString(), ""));
+    }
+}
+
